c3dfae52db86ce73d294abefec7b940ec6c41a24
#xoxor4d <r4d0xzz@live.de>
2025-03-24 18:26:49 +0100

    change module loading order

--- src/components/loader.cpp
+++ src/components/loader.cpp
@@ -10,17 +10,17 @@ namespace components
 		mem_allocator_.clear();
 		_register(new interfaces());
 		_register(new flags());
+		_register(new remix_api());
+		_register(new imgui());
 		_register(new game_settings());
 		_register(new choreo_events());
 		_register(new sound_events());
 		_register(new main_module());
 		_register(new model_render());
-		_register(new remix_api());
 		_register(new remix_vars());
 		_register(new remix_rayportal());
 		_register(new remix_lights());
 		_register(new map_settings());
-		_register(new imgui());
 
 		XASSERT(MH_EnableHook(MH_ALL_HOOKS) != MH_STATUS::MH_OK);
 	}
