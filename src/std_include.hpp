9065f90b999d0ae2b510da904f9fed30572fa782
#xoxor4d <r4d0xzz@live.de>
2026-01-08 19:50:56 +0100

    bump version to 2.2.3

--- src/std_include.hpp
+++ src/std_include.hpp
@@ -6,7 +6,7 @@
 
 constexpr auto COMP_MOD_VERSION_MAJOR = 2;
 constexpr auto COMP_MOD_VERSION_MINOR = 2;
-constexpr auto COMP_MOD_VERSION_PATCH = 0;
+constexpr auto COMP_MOD_VERSION_PATCH = 3;
 
 #define COMPMOD_ASSET_DIR "\\portal2-rtx\\"
 
